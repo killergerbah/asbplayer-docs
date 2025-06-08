c56fa1ca71a0896f9447ab46590cd9fb56acfc2d
R-J Lim <kgerbil@gmail.com>
2025-06-08 11:34:58 +0900

    Add search

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -111,6 +111,23 @@ const config: Config = {
       darkTheme: prismThemes.dracula,
     },
   } satisfies Preset.ThemeConfig,
+  themes: [
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      {
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+
+        // For Docs using Chinese, it is recomended to set:
+        language: ["en"],
+
+        // If you're using `noIndex: true`, set `forceIgnoreNoIndex` to enable local index:
+        forceIgnoreNoIndex: true,
+      },
+    ],
+  ],
 };
 
 export default config;
